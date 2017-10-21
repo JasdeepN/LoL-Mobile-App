ade6cbb442cd1a501784537b8d524eb36bfb6afb
Jasdeep Nijjar <jasdeepn4@hotmail.com>
2017-10-21 19:28:12 -0400

    inital commit, new project for mobile app

---
+++ MobileApp/app/src/main/java/uoit/csci4100u/mobileapp/Main.java
@@ -0,0 +1,13 @@
+package uoit.csci4100u.mobileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
